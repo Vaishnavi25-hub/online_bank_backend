b03750e95f49fe146aba477def38aa8f67a225f2
Vaishnavi25-hub <vaishnavimd2512@gmail.com>
2023-08-11 12:52:28 +0530

    new

---
+++ src/main/java/com/batch8group4/onlinebank/model/Admin.java
@@ -0,0 +1,14 @@
+package com.batch8group4.onlinebank.model;
+import java.util.*;
+import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import javax.persistence.Transient;
+@Entity
+@Table(name="Admin")
+public class Admin {
+	
+
+}
