21f8ad248ec27c69a03a07fb80802ba9901fbecc
Vivek rawat <54075807+vvkrawt@users.noreply.github.com>
2023-08-11 15:30:08 +0530

    Add files via upload

---
+++ src/main/java/com/batch8group4/onlinebank/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.batch8group4.onlinebank.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.batch8group4.onlinebank.model.Account;
+import com.batch8group4.onlinebank.model.Customer;
+import com.batch8group4.onlinebank.service.AccountService;
+import com.batch8group4.onlinebank.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class AccountController {
+	@Autowired
+	AccountService accountService;
+	
+	@GetMapping("/account")
+	public List<Account> getAllAccountsResponse ()
+	{
+		return accountService.getAllAccounts();
+	}
+	
+	@GetMapping("/account/{accountNumber}")
+	public Optional<Account> getAccountByAccountNumberResponse(@PathVariable String accountNumber)
+	{
+		return accountService.getAccountByAccountNumber(accountNumber);
+	}
+}
